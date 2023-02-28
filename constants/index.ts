a8ec61dd1f7653b4d327bd56a13c13d02815d0c2
guxuerui <im.guxuerui@gmail.com>
2023-02-28 21:50:21 +0800

    feat(constants): set static table data

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,80 @@
+import type { Table } from '~/types/index'
+
 export const appName = 'Calculate Anything'
+
 export const appDescription = 'Calculate Anything'
+
+export const calculateTable: Table[] = [
+  {
+    title: 'Clear',
+    value: 'clear',
+  },
+  {
+    title: '%',
+    value: '%',
+  },
+  {
+    title: '÷',
+    value: '/',
+  },
+  {
+    title: '7',
+    value: 7,
+  },
+  {
+    title: '8',
+    value: 8,
+  },
+  {
+    title: '9',
+    value: 9,
+  },
+  {
+    title: 'x',
+    value: '*',
+  },
+  {
+    title: '4',
+    value: 4,
+  },
+  {
+    title: '5',
+    value: 5,
+  },
+  {
+    title: '6',
+    value: 6,
+  },
+  {
+    title: '-',
+    value: '-',
+  },
+  {
+    title: '1',
+    value: 1,
+  },
+  {
+    title: '2',
+    value: 2,
+  },
+  {
+    title: '3',
+    value: 3,
+  },
+  {
+    title: '+',
+    value: '+',
+  },
+  {
+    title: '0',
+    value: 0,
+  },
+  {
+    title: '.',
+    value: '.',
+  },
+  {
+    title: '=',
+    value: '=',
+  },
+]
